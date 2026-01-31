fd02fe7d6c654aa7101834711d3f2e95a3fcb3a7
Qi Wang <qi_wang_zz@outlook.com>
2026-02-01 01:19:50 +0800

    ncurses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate ncurses;
+
+use ncurses::*;
+
 fn main() {
-    println!("Hello, world!");
+    // Init ncurses
+    initscr();              // Initialize the window
+    raw();                  // Disable line buffering
+    keypad(stdscr(), true); // Enable function keys
+    noecho();               // Disable echoing of input characters
+
+    // Print text
+    let _ = addstr("Hello, Xegue!\n");
+    let _ = addstr("Press any key to exit...");
+
+    // Refresh the screen
+    refresh();
+
+    // Wait for user input
+    getch();
+
+    // End ncurses mode
+    endwin();
 }
