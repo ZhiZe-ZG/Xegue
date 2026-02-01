5dca2a0aa55af9c163016230fd4ce1353816cdd3
Qi Wang <qi_wang_zz@outlook.com>
2026-02-01 13:41:05 +0800

    feat: update dependencies in Cargo.toml and enhance map generation logic in generate_map.rs

--- src/map/generate_map.rs
+++ src/map/generate_map.rs
@@ -1,4 +1,5 @@
 use glam::IVec2;
+use rand::Rng;
 
 use crate::map::map_grid::MapGrid;
 use crate::map::map_structure::{put_room, Room};
@@ -8,6 +9,7 @@ pub fn generate_map(length: usize, width: usize, _height: usize) -> MapGrid {
 	if length < 3 || width < 3 {
 		return map;
 	}
+	let mut rng = rand::rng();
 
 	let length_i = length as i32;
 	let width_i = width as i32;
@@ -15,30 +17,23 @@ pub fn generate_map(length: usize, width: usize, _height: usize) -> MapGrid {
 	let bsze_y = length_i / 3;
 
 	for i in 0..9 {
-		let top_x = (i % 3) as i32 * bsze_x;
+		let top_x = (i % 3) as i32 * bsze_x + 1;
 		let top_y = (i / 3) as i32 * bsze_y;
-		let mut pos_x = top_x + 1;
-		let mut pos_y = top_y + 1;
 
-		if pos_x < 1 {
-			pos_x = 1;
-		}
-		if pos_y < 1 {
-			pos_y = 1;
+		let max_room_x = (bsze_x - 4).max(0);
+		let max_room_y = (bsze_y - 4).max(0);
+		if max_room_x == 0 || max_room_y == 0 {
+			continue;
 		}
 
-		let mut size_x = (bsze_x - 1).max(0);
-		let mut size_y = (bsze_y - 1).max(0);
-		let max_x = (width_i - 1 - pos_x).max(0);
-		let max_y = (length_i - 1 - pos_y).max(0);
-		if size_x > max_x {
-			size_x = max_x;
-		}
-		if size_y > max_y {
-			size_y = max_y;
-		}
+		let size_x = rng.random_range(0..=max_room_x) + 4;
+		let size_y = rng.random_range(0..=max_room_y) + 4;
+		let max_pos_x = (bsze_x - size_x).max(0);
+		let max_pos_y = (bsze_y - size_y).max(0);
+		let pos_x = top_x + rng.random_range(0..=max_pos_x);
+		let pos_y = top_y + rng.random_range(0..=max_pos_y);
 
-		if size_x < 3 || size_y < 3 {
+		if pos_y <= 0 || pos_y >= length_i - 1 {
 			continue;
 		}
 
